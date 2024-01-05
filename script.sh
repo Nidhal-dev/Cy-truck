8e044e07d8d3ea3246b4d5935c1d835362af5eea
Nidhal-dev <65866667+Nidhal-dev@users.noreply.github.com>
2024-01-05 12:26:48 +0100

    updated original script.sh
    - Traitement D1
    - correction des vérifications

--- script.sh
+++ script.sh
@@ -8,34 +8,34 @@ echo "rentrer le tri voulu :"
 
 read tri
 
-if [ -f "/home/nidou/cy-truck/progc/prog.c" ];then
+if [ -f "/home/nidou/cy-truck/progc" ];then
 echo "Le .c existe";
 fi
 
 
-if [ ! -f "/home/nidou/cy-truck/progc/prog.c" ];then
+if [ ! -f "/home/nidou/cy-truck/progc" ];then
 echo "Le .c n'existe pas, compilation";
-fi
-
-
-if [ -f "/home/nidou/cy-truck/progc/prog.c" ];then
-echo "Le .c existe";
 fi
 
 if [ ! -d "/home/nidou/cy-truck/image" ];then
 echo "Création de image";
-mkdir /home/cy-truck/image
+mkdir /home/nidou/cy-truck/image
 fi
 
 if [ -d "/home/nidou/cy-truck/temp" ];then
 echo "supression contenu de temp";
-rm -r /home/cy-truck/temp
-mkdir /home/cy-truck/temp
+rm -r /home/nidou/cy-truck/temp
+mkdir /home/nidou/cy-truck/temp
 fi
 
 if [ ! -d "/home/nidou/cy-truck/temp" ];then
 echo "Création de temp";
-mkdir /home/cy-truck/temp
+mkdir /home/nidou/cy-truck/temp
 fi
 
-awk -F';' -f 'data.csv' " print $6" test.txt
+start_time=$(date +%s.%N)
+awk -F ';' '!seen[$6,$1]++ { count[$6]++ } END { for (name in count) print name ";" count[name] }' data.csv | sort --parallel=8 -t';' -k2,2nr | head -n 10
+end_time=$(date +%s.%N)
+
+elapsed_time=$(echo "$end_time - $start_time" | bc)
+echo "Temps écoulé : $elapsed_time secondes"
