7a50c3c8271342a9fb5fd66ce6cd128d90b9f4ce
Nidhal-dev <65866667+Nidhal-dev@users.noreply.github.com>
2024-01-08 23:32:17 +0100

    Update script.sh
    Traitement D2 supplémentaire (imparfait)

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#vérifications
 echo "rentrer le chemin du fichier CSV :"
 
 read chemin
@@ -33,9 +33,27 @@ echo "Création de temp";
 mkdir /home/nidou/cy-truck/temp
 fi
 
+#traitement D1
 start_time=$(date +%s.%N)
 awk -F ';' '!seen[$6,$1]++ { count[$6]++ } END { for (name in count) print name ";" count[name] }' data.csv | sort --parallel=8 -t';' -k2,2nr | head -n 10
 end_time=$(date +%s.%N)
 
 elapsed_time=$(echo "$end_time - $start_time" | bc)
 echo "Temps écoulé : $elapsed_time secondes"
+
+#traitement D2
+awk -F ';' '
+{
+    driver = $6;  # Colonne du conducteur
+    distance = $5;  # Colonne de la distance
+    gsub(",", ".", distance);  # Remplace les virgules par des points
+
+    if (driver != "" && distance != "") {
+        sum[driver] += distance;
+    }
+}
+END {
+    for (driver in sum) {
+        printf "%s;%.6f\n", driver, sum[driver];
+    }
+}' data.csv | sort -t';' -k2,2nr | head -n 10
